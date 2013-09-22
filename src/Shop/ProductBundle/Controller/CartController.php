3d57e5ce6674775a86834316f452ee07839b1b18
Ivnmaksimovic <poslatiivanu@yahoo.com>
2013-09-23 01:43:55 +0200

    cart increase qty, cart decrease qty, cart delete product

--- src/Shop/ProductBundle/Controller/CartController.php
+++ src/Shop/ProductBundle/Controller/CartController.php
@@ -25,7 +25,7 @@ class CartController extends Controller
      *
      * @Route("/add", name="cart_add")
      * @MEthod("GET")
-     * @Template("ProductBundle:Cart:add.html.twig")
+     * @Template("ProductBundle:Cart:cart.html.twig")
      */
     public function addAction(Request $request)
     {
@@ -54,7 +54,6 @@ class CartController extends Controller
         }
         else
         {
-            $productId = $selectedProductId;
             $existingQty = $existingProduct->getQuantity();
             $qty = $existingQty + 1;
 
@@ -75,20 +74,90 @@ class CartController extends Controller
 
     }
 
+    /**
+     * Remove selected product from cart or decrease quantity by 1
+     *
+     * @Route("/remove", name="cart_remove")
+     * @MEthod("GET")
+     * @Template("ProductBundle:Cart:cart.html.twig")
+     */
+    public function removeAction(Request $request)
+    {
+        $selectedProductId = $request->get('productId');
+        $sessionId = $request->getSession()->getId();
+
+        $criteria = array(
+            'product' => $selectedProductId,
+            'sessionId' => $sessionId,
+        );
+        $em = $this->getDoctrine()->getManager();
+        $existingProduct = $em->getRepository('ProductBundle:Cart')->findOneBy($criteria);
+        $existingQty = $existingProduct->getQuantity();
+
+        if($existingQty == 1)
+        {
+            $em->remove($existingProduct);
+            $em->flush();
+        }
+        else
+        {
+            $qty = $existingQty - 1;
+            $existingProduct->setQuantity($qty);
+
+            $em->persist($existingProduct);
+            $em->flush();
+        }
+
+        $userCart = $em->getRepository('ProductBundle:Cart')->findBy(array(
+            'sessionId' => $sessionId,
+        ));
 
 
+        return array(
+            'entities' => $userCart,
+        );
+
+    }
+
+    /**
+     * Deletes product from cart.
+     *
+     * @Route("/delete", name="cart_delete")
+     * @Method("GET")
+     * @Template("ProductBundle:Cart:cart.html.twig")
+     */
+    public function deleteAction(Request $request)
+    {
+        $selectedProductId = $request->get('productId');
+        $sessionId = $request->getSession()->getId();
+
+        $criteria = array(
+            'product' => $selectedProductId,
+            'sessionId' => $sessionId,
+        );
+        $em = $this->getDoctrine()->getManager();
+        $existingProduct = $em->getRepository('ProductBundle:Cart')->findOneBy($criteria);
 
 
+        $em->remove($existingProduct);
+        $em->flush();
+
+        $userCart = $em->getRepository('ProductBundle:Cart')->findBy(array(
+            'sessionId' => $sessionId,
+        ));
 
 
-
+        return array(
+            'entities' => $userCart,
+        );
+    }
 
     /**
      * Lists all Cart entities.
      *
      * @Route("/", name="cart")
      * @Method("GET")
-     * @Template()
+     * @Template("ProductBundle:Cart:index.html.twig")
      */
     public function indexAction(Request $request)
     {
@@ -105,69 +174,33 @@ class CartController extends Controller
             'entities' => $userCart,
         );
     }
-    /**
-     * Creates a new Cart entity.
-     *
-     * @Route("/", name="cart_create")
-     * @Method("POST")
-     * @Template("ProductBundle:Cart:new.html.twig")
-     */
-    public function createAction(Request $request)
-    {
-        $entity = new Cart();
-        $form = $this->createCreateForm($entity);
-        $form->handleRequest($request);
 
-        if ($form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($entity);
-            $em->flush();
 
-            return $this->redirect($this->generateUrl('cart_show', array('id' => $entity->getId())));
-        }
 
-        return array(
-            'entity' => $entity,
-            'form'   => $form->createView(),
-        );
-    }
 
-    /**
-    * Creates a form to create a Cart entity.
-    *
-    * @param Cart $entity The entity
-    *
-    * @return \Symfony\Component\Form\Form The form
-    */
-    private function createCreateForm(Cart $entity)
-    {
-        $form = $this->createForm(new CartType(), $entity, array(
-            'action' => $this->generateUrl('cart_create'),
-            'method' => 'POST',
-        ));
 
-        $form->add('submit', 'submit', array('label' => 'Create'));
 
-        return $form;
-    }
 
-    /**
-     * Displays a form to create a new Cart entity.
-     *
-     * @Route("/new", name="cart_new")
-     * @Method("GET")
-     * @Template()
-     */
-    public function newAction()
-    {
-        $entity = new Cart();
-        $form   = $this->createCreateForm($entity);
 
-        return array(
-            'entity' => $entity,
-            'form'   => $form->createView(),
-        );
-    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     /**
      * Finds and displays a Cart entity.
@@ -194,127 +227,4 @@ class CartController extends Controller
         );
     }
 
-    /**
-     * Displays a form to edit an existing Cart entity.
-     *
-     * @Route("/{id}/edit", name="cart_edit")
-     * @Method("GET")
-     * @Template()
-     */
-    public function editAction($id)
-    {
-        $em = $this->getDoctrine()->getManager();
-
-        $entity = $em->getRepository('ProductBundle:Cart')->find($id);
-
-        if (!$entity) {
-            throw $this->createNotFoundException('Unable to find Cart entity.');
-        }
-
-        $editForm = $this->createEditForm($entity);
-        $editForm->remove('sessionId');
-        $deleteForm = $this->createDeleteForm($id);
-
-        return array(
-            'entity'      => $entity,
-            'edit_form'   => $editForm->createView(),
-            'delete_form' => $deleteForm->createView(),
-        );
-    }
-
-    /**
-    * Creates a form to edit a Cart entity.
-    *
-    * @param Cart $entity The entity
-    *
-    * @return \Symfony\Component\Form\Form The form
-    */
-    private function createEditForm(Cart $entity)
-    {
-        $form = $this->createForm(new CartType(), $entity, array(
-            'action' => $this->generateUrl('cart_update', array('id' => $entity->getId())),
-            'method' => 'PUT',
-        ));
-
-        $form->add('submit', 'submit', array('label' => 'Update'));
-        $form->remove('sessionId');
-
-        return $form;
-    }
-    /**
-     * Edits an existing Cart entity.
-     *
-     * @Route("/{id}", name="cart_update")
-     * @Method("PUT")
-     * @Template("ProductBundle:Cart:edit.html.twig")
-     */
-    public function updateAction(Request $request, $id)
-    {
-        $em = $this->getDoctrine()->getManager();
-
-        $entity = $em->getRepository('ProductBundle:Cart')->find($id);
-
-        if (!$entity) {
-            throw $this->createNotFoundException('Unable to find Cart entity.');
-        }
-
-        $deleteForm = $this->createDeleteForm($id);
-        $editForm = $this->createEditForm($entity);
-
-        $editForm->handleRequest($request);
-
-        if ($editForm->isValid()) {
-            $em->flush();
-
-            return $this->redirect($this->generateUrl('cart_edit', array('id' => $id)));
-        }
-
-        return array(
-            'entity'      => $entity,
-            'edit_form'   => $editForm->createView(),
-            'delete_form' => $deleteForm->createView(),
-        );
-    }
-    /**
-     * Deletes a Cart entity.
-     *
-     * @Route("/{id}", name="cart_delete")
-     * @Method("DELETE")
-     */
-    public function deleteAction(Request $request, $id)
-    {
-        $form = $this->createDeleteForm($id);
-        $form->handleRequest($request);
-
-        if ($form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $entity = $em->getRepository('ProductBundle:Cart')->find($id);
-
-            if (!$entity) {
-                throw $this->createNotFoundException('Unable to find Cart entity.');
-            }
-
-            $em->remove($entity);
-            $em->flush();
-        }
-
-        return $this->redirect($this->generateUrl('cart'));
-    }
-
-    /**
-     * Creates a form to delete a Cart entity by id.
-     *
-     * @param mixed $id The entity id
-     *
-     * @return \Symfony\Component\Form\Form The form
-     */
-    private function createDeleteForm($id)
-    {
-        return $this->createFormBuilder()
-            ->setAction($this->generateUrl('cart_delete', array('id' => $id)))
-            ->setMethod('DELETE')
-            ->add('submit', 'submit', array('label' => 'Delete'))
-            ->getForm()
-        ;
-    }
 }
